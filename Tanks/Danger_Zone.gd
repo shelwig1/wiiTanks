7092c434993a4475f4ac0a358c29073d3c3a5a00
Sean <sean.helwig@gmail.com>
2024-11-17 15:44:56 -0500

    exposure shape working 1st draft

--- Tanks/Danger_Zone.gd
+++ Tanks/Danger_Zone.gd
@@ -3,9 +3,17 @@ extends Node3D
 @onready var radar = $Radar
 @onready var exposure_shape = $Exposure_Shape
 
+@onready var marker = preload("res://Objects/Marker.tscn")
+const TEST_MATERIAL = preload("res://Objects/test_material.tres")
+
 var timer : float = 0.0
-var delay_time : float = 0.05
+var delay_time : float = 0.01
 var index : int = 0
+
+var marker_count : int = 0
+var coords = PackedVector3Array()
+
+var lastSweep
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Make radar look at the zero rotation
@@ -14,26 +22,69 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if radar.is_colliding():
+			var point = radar.get_collision_point()
+			marker_count += 1
+			coords.push_back(to_local(point))
+	if marker_count == 720:
+			make_mesh(coords)
+			coords.clear()
+			marker_count = 0 
+			
+	radar.rotation_degrees.y -= 0.5
 	
-	timer += delta
-	
-	if index > 35:
-		index = 0
+	"timer += delta
 	
 	if timer >= delay_time:
 		if radar.is_colliding():
-			exposure_shape.update_vertex(radar.get_collision_point(), index)
+			var point = radar.get_collision_point()
+			marker_count += 1
+			coords.push_back(to_local(point))
 			
-		print("Radar rotation: ", radar.rotation_degrees.y)
-		radar.rotation_degrees.y += 10
-		timer = 0
-		index += 1
+			
+		if marker_count == 37:
+			make_mesh(coords)
+			coords.clear()
+			marker_count = 0 
 
-# sweep the ray cast around every X seconds
-	#predefine the angels we care about
+		radar.rotation_degrees.y -= 10
 	
-# Use the points we hit to redefine that part of the shape
+		timer = 0
+	"	
 
-# make a mesh with X number of points - the number is constant, because the radar only goes to x number of fixed points
 
-# when we get the point, go into the mesh and change that
+func make_mesh(coords):
+	print("Made mesh")
+	
+	if lastSweep != null:
+		lastSweep.queue_free()
+	
+	var indices = PackedInt32Array()
+	for i in range(coords.size() - 1):
+		indices.push_back(i)
+		indices.push_back(i + 1)
+	
+	# Iitialize the ArrayMesh.
+	var arr_mesh = ArrayMesh.new()
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = coords
+	arrays[Mesh.ARRAY_INDEX] = indices
+
+	# Create the Mesh.
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, arrays)
+	
+	var m = MeshInstance3D.new()
+	m.mesh = arr_mesh
+	m.top_level = true
+	self.add_child(m)
+	
+	lastSweep = m
+	
+	"if exposure_shape.mesh != null:
+		exposure_shape.mesh = null
+	
+	exposure_shape.mesh = arr_mesh"
+	
+	coords = []
+
--- Tanks/Exposure_Shape.gd
+++ Tanks/Exposure_Shape.gd
@@ -1,37 +1,37 @@
 extends MeshInstance3D
 
+const TEST_MATERIAL = preload("res://Objects/test_material.tres")
 # Make it accessible to the Danger_Zone calculator
 var vertices = []
 
 func _ready():
-	"vertices = [
-		Vector3(0,0,0)
-	]"
+	pass
 	
-	for i in range(36):
-		if i < 10:
-			vertices.append(Vector3(30,0,30))
-		if i >= 10 and i < 25:
-			vertices.append(Vector3(0,0,30))
-		else:
-			vertices.append(Vector3(30,0,0))
+	"vertices.push_back(Vector3(0, 0, -100))
+	vertices.push_back(Vector3(100, 0, 0))
+	vertices.push_back(Vector3(0, -0, 100))"	
+"
+	vertices.push_back(Vector3(176.6211, 5, 101.9722))
+	vertices.push_back(Vector3(-176.6211, 5, 101.9722))
+	vertices.push_back(Vector3(-0, 5, -58.21865))
 	
-	update_mesh()
+
+
+	# Initialize the ArrayMesh.
+	var arr_mesh = ArrayMesh.new()
+	var arrays = []
+	arrays.resize(Mesh.ARRAY_MAX)
+	arrays[Mesh.ARRAY_VERTEX] = vertices
+
+	# Create the Mesh.
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	var m = MeshInstance3D.new()
+	m.mesh = arr_mesh
+	
+	self.add_child(m)"
+	#update_mesh()
 
 func _process(delta):
 	pass
 
-func update_vertex(location : Vector3, index : int) -> void:
-	vertices[index] = location
-	update_mesh()
-	print("Updated ", index, " with location ", location)
 
-func update_mesh() -> void:
-	var st = SurfaceTool.new()
-	st.begin(Mesh.PRIMITIVE_TRIANGLES)
-	
-	for vertex in vertices:
-		st.add_vertex(vertex)
-		
-	mesh = st.commit()
-	self.mesh = mesh
